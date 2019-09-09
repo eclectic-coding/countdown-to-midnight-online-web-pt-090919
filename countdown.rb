0bfa247e54b6d020d19afbe1e8a651c379cf7e8c
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 21:04:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,13 @@ def countdown(numbers)
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(numbers)
+  while numbers > 0
+    puts "#{numbers} SECOND(S)!"
+    numbers -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
 
