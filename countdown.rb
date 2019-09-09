50d1e8c58666fd36e25ef49a63f77ab9a3910adb
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 21:03:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
+def countdown(numbers)
+  while numbers > 0
+    puts "#{numbers} SECOND(S)!"
+    numbers -= 1
   end
   "HAPPY NEW YEAR!"
 end
