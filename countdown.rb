314713747372e75f21ffbea3d05e03b5dbbe1a6d
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 21:01:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
-def countdown
-  number = 10 
-  
+def countdown(numbers)
   while number > 0 
     puts
     "#{number} SECOND(S)!"
     number -+ 1 
   end
+  puts 
 end
