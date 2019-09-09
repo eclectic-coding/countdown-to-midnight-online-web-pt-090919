d91ddd4fcbfedff6708d58b6b8a8788209ea980c
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 21:02:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,8 @@
 
 def countdown(number)
   while number > 0 
-    puts
-    "#{number} SECOND(S)!"
-    number -+ 1 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
   end
   puts "HAPPY NEW YEAR!"
 end
